fca4658dd5ac0b15e80a9d1a0721e5db6f188161
Martin Larsson <martin.larsson.arbete@outlook.com>
2022-09-21 21:32:24 +0200

    refactor: cleaned up console.log in websocket

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,7 +1,8 @@
 const loginForm = document.getElementById("loginForm");
-
+const loginBtn = document.getElementById("loginBtn")
 function submitLoginForm(e) {
     e.preventDefault();
+    loginBtn.disabled = true;
     const formEntries = ObjectifyEntriesAndStringify(e.target)
     const options = setFetchPostOptions(formEntries)
     fetch('/login', options)
@@ -9,6 +10,7 @@ function submitLoginForm(e) {
         .then(data => {
             if (data.redirectTo) {
                 location.assign(data.redirectTo)
+                // loginBtn.disabled = false;
               }
             if (data.err) {
                 throw data.err;
@@ -16,6 +18,7 @@ function submitLoginForm(e) {
         }).catch(err => {
             //console.log(err, "33");
             manageErrorAndAppendToPopupBox(err)
+            loginBtn.disabled = false;
         });
 }
 
